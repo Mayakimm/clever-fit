55ff70bb7c94b06122e0692f22059f85f32c0b29
matt shilling <matthewds@sky.com>
2024-07-16 13:52:41 +0100

    Done the routes and done some of the action

--- app/controllers/workout_exercises_controller.rb
+++ app/controllers/workout_exercises_controller.rb
@@ -1,2 +1,10 @@
 class WorkoutExercisesController < ApplicationController
+
+  def summary
+    @workout = Workout.find(params[:id])
+    @exercises = @workout.exercise
+    @start_time = datetime
+    @end_time = datetime
+    @duration = (@endtime - @start_time) / 3600
+  end
 end
