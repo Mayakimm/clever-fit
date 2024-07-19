139b3b40433b58f5ea9d69d87cc8b3304bbf729e
Maya Kim <mayagy.kim@gmail.com>
2024-07-19 12:37:38 +0200

    fixed next button

--- app/controllers/workout_exercises_controller.rb
+++ app/controllers/workout_exercises_controller.rb
@@ -3,7 +3,9 @@ class WorkoutExercisesController < ApplicationController
     @workout_exercise = WorkoutExercise.find(params[:id])
     # @workout_all_exercises = @workout.workout_exercises
     @current_workout_exercises = @workout_exercise.workout.workout_exercises
-    @current_exercise = @current_workout_exercises.first.exercise
+    @current_index = @current_workout_exercises.find_index(@workout_exercise)
+    @current_exercise = @current_workout_exercises[@current_index].exercise
+    @max_index = @current_workout_exercises.count - 1
     # @current_workout_exercise_id = params[:id].to_i
     # until @current_workout_exercise_id == @workout.workout_exercises.last.id - 1
     #   @next_workout_exercise = @workout.workout_exercises.find(@current_workout_exercise_id+1)
