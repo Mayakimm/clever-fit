ee6e3788e65a4b6d74f6a5c20b513f39986e07cd
Maya Kim <mayagy.kim@gmail.com>
2024-07-22 17:39:56 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2024_07_22_135235) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +21,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_22_135235) do
     t.datetime "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
     t.datetime "last_update_time"
     t.float "calories_burnt"
+    t.string "status"
     t.index ["profile_id"], name: "index_day_summaries_on_profile_id"
   end
 
