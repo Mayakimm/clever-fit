34d9beca029e22898a533abd18cbdaae9675e7a8
Maya Kim <mayagy.kim@gmail.com>
2024-07-24 15:02:57 +0200

    icon design and seed done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_23_154000) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_24_122016) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -122,6 +122,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_23_154000) do
     t.datetime "end_time"
     t.string "muscle_group"
     t.integer "duration"
+    t.text "icon"
     t.index ["user_id"], name: "index_workouts_on_user_id"
   end
 
